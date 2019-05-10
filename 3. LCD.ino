d5ac62ff8e928fd5348bedcaa9e6c36f276e78d3
fbqweasd <fbqweasd2911@gmail.com>
2019-05-10 09:46:02 +0900

    LCD update

---
+++ 3. LCD.ino
@@ -0,0 +1,71 @@
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x3f, 16, 2);
+
+// 습도 센서 핀
+int water_sensor = A0; 
+
+// 모터 제어 핀들 //
+int motor_1 = 2;
+int motor_2 = 3;
+/* ------------- */
+
+//LED 제어핀
+int LED_R = 8;
+int LED_G = 9;
+int LED_B = 10;
+
+// 입력 데이터 저장
+int water_data;
+
+void setup() {
+  //lcd 활설화
+  lcd.begin();
+
+  // 습도 센서 set
+  pinMode(water_sensor,INPUT);
+
+  // 모터 핀 set
+  pinMode(motor_1,OUTPUT);
+  pinMode(motor_2,OUTPUT);
+  pinMode(motor_a,OUTPUT);
+  pinMode(motor_b,OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+ water_data = analogRead(water_sensor);
+
+  if( water_data > 300 ){ // 촉촉함 
+    digitalWrite(motor_1,LOW); //모터 정지
+    digitalWrite(motor_2,HIGH);
+
+    digitalWrite(LED_R,LOW); // 초록색 LED on
+    digitalWrite(LED_G,HIGH);
+    digitalWrite(LED_B,LOW);
+
+    lcd.clear();
+    lcd.print("촉촉해염");
+  }
+  else if (water_data > 700){ // 경고
+    digitalWrite(motor_1,LOW); // 모터 정지
+    digitalWrite(motor_2,HIGH);
+  
+    digitalWrite(LED_R,HIGH); // 노란색 LED on
+    digitalWrite(LED_G,HIGH);
+    digitalWrite(LED_B,LOW);
+    
+    lcd.clear();
+    lcd.print("곧 물이 드갈거임 물통 체크하셈");
+  }
+  else{ // 물 필요함
+    digitalWrite(motor_1,HIGH); //모터 활성화
+    digitalWrite(motor_2,LOW);
+    
+    digitalWrite(LED_R,LOW); //빨간 LED on
+    digitalWrite(LED_G,HIGH);
+    digitalWrite(LED_B,LOW);
+
+    lcd.clear();
+    lcd.print("물 필요함!@#$&*");
+  }
+}

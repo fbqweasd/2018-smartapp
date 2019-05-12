87bdbf5d3402f30ba99ca1a93dd8f8634fb380c1
fbqweasd <fbqweasd2911@gmail.com>
2019-05-12 13:26:01 +0900

    img add

--- 2. LED_boom_boom.ino
+++ 2. LED_boom_boom.ino
@@ -1,11 +1,6 @@
 // 습도 센서 핀
 int water_sensor = A0; 
 
-// 모터 제어 핀들 //
-int motor_1 = 2;
-int motor_2 = 3;
-/* ------------- */
-
 //LED 제어핀
 int LED_R = 8;
 int LED_G = 9;
@@ -18,11 +13,10 @@ void setup() {
   // 습도 센서 set
   pinMode(water_sensor,INPUT);
 
-  // 모터 핀 set
-  pinMode(motor_1,OUTPUT);
-  pinMode(motor_2,OUTPUT);
-  pinMode(motor_a,OUTPUT);
-  pinMode(motor_b,OUTPUT);
+  // LED pin SET
+  pinMode(LED_R,OUTPUT);
+  pinMode(LED_B,OUTPUT);
+  pinMode(LED_G,OUTPUT);
 }
 
 // the loop function runs over and over again forever
@@ -30,24 +24,15 @@ void loop() {
  water_data = analogRead(water_sensor);
 
   if( water_data > 300 ){ // 촉촉함 
-    digitalWrite(motor_1,LOW); //모터 정지
-    digitalWrite(motor_2,HIGH);
-
     digitalWrite(LED_R,LOW); // 초록색 LED on
     digitalWrite(LED_G,HIGH);
     digitalWrite(LED_B,LOW);
   }
   else if (water_data > 700){ // 경고
-    digitalWrite(motor_1,LOW); // 모터 정지
-    digitalWrite(motor_2,HIGH);
-  
     digitalWrite(LED_R,HIGH); // 노란색 LED on
     digitalWrite(LED_G,HIGH);
     digitalWrite(LED_B,LOW);}
   else{ // 물 필요함
-    digitalWrite(motor_1,HIGH); //모터 활성화
-    digitalWrite(motor_2,LOW);
-    
     digitalWrite(LED_R,LOW); //빨간 LED on
     digitalWrite(LED_G,HIGH);
     digitalWrite(LED_B,LOW);
